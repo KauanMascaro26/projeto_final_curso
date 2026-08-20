3d8cf152b38dc7a0357e036936b5e63558ceb55d
Kauan Mascaro <kauanmascaro@gmail.com>
2026-08-20 01:02:19 -0300

    feat: configure Google Maps integration

--- mobile/descarte_inteligente/android/app/build.gradle.kts
+++ mobile/descarte_inteligente/android/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -36,6 +37,13 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+secrets {
+    propertiesFileName = "secrets.properties"
+    defaultPropertiesFileName = "local.defaults.properties"
+    ignoreList.add("keyAlias")
+    ignoreList.add("keyPassword")
 }
 
 kotlin {
